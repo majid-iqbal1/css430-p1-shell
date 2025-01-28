b201f10d35be1d157cc2e55c37e938626d2477f5
Majid Iqbal <majidi2@uw.edu>
2025-01-27 18:32:36 -0800

    UPDATED shell.h

--- shell.h
+++ shell.h
@@ -8,18 +8,37 @@
 #include <stdlib.h>  // calloc
 #include <string.h>  // strcmp
 #include <unistd.h>  // execvp
+#include <sys/wait.h> // wait
+#include <ctype.h>   // isspace
 
 #define MAXLINE 80
 #define PROMPT "osh> "
+#define HISTORY_SIZE 10
 
 #define RD 0
 #define WR 1
+
+typedef struct {
+    char *args[MAXLINE/2 + 1];  
+    int arg_count;              
+    bool background;            
+    bool pipe_exists;           
+    char *input_file;          
+    char *output_file;        
+} Command;
 
 bool equal(char *a, char *b);
 int fetchline(char **line);
 int interactiveShell();
 int runTests();
 void processLine(char *line);
-int main();
+void parseCommand(char *line, Command *cmd);
+void executeCommand(Command *cmd);
+void addToHistory(char *command);
+void executeHistoryCommand();
+void executePipedCommand(Command *cmd1, Command *cmd2);
+void handleRedirection(Command *cmd);
+void ascii_art();
+void cleanup_command(Command *cmd);
 
 #endif
